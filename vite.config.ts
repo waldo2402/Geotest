eb5908cf7d67238007abde622b11ba1838929d21
oswaldo <og64223@gmail.com>
2025-09-05 00:03:12 -0600

    Primer commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+<<<<<<< HEAD
+=======
+import react from '@vitejs/plugin-react';
+>>>>>>> 43f92ad (Primer commit)
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+<<<<<<< HEAD
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
@@ -13,5 +18,18 @@ export default defineConfig(({ mode }) => {
           '@': path.resolve(__dirname, '.'),
         }
       }
+=======
+        plugins: [react()],
+        base: '/Geotest/', // <- Importante para GitHub Pages
+        define: {
+            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, '.'),
+            }
+        }
+>>>>>>> 43f92ad (Primer commit)
     };
 });
